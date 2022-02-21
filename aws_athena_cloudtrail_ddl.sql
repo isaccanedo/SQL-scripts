e802351abd443f2a56ce472eb06d54bcaf81126e
isaccanedo <isaccanedo@gmail.com>
2022-02-21 08:16:57 -0300

    Initial commit

---
+++ aws_athena_cloudtrail_ddl.sql
@@ -0,0 +1,69 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2020-01-01 15:38:58 +0000 (Wed, 01 Jan 2020)
+--
+--  vim:ts=4:sts=4:sw=4:et:filetype=sql
+--
+--  https://github.com/harisekhon/sql
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn
+--  and optionally send me feedback to help improve or steer this or other code I publish
+--
+--  https://www.linkedin.com/in/harisekhon
+--
+
+-- replace <MY_BUCKET> and <MY_ACCOUNT_NUMBER> on last line
+
+CREATE EXTERNAL TABLE cloudtrail_logs (
+    eventVersion STRING,
+    userIdentity STRUCT<
+        type: STRING,
+        principalId: STRING,
+        arn: STRING,
+        accountId: STRING,
+        invokedBy: STRING,
+        accessKeyId: STRING,
+        userName: STRING,
+        sessionContext: STRUCT<
+            attributes: STRUCT<
+                mfaAuthenticated: STRING,
+                creationDate: STRING>,
+            sessionIssuer: STRUCT<
+                type: STRING,
+                principalId: STRING,
+                arn: STRING,
+                accountId: STRING,
+                userName: STRING>>>,
+    eventTime STRING,
+    eventSource STRING,
+    eventName STRING,
+    awsRegion STRING,
+    sourceIpAddress STRING,
+    userAgent STRING,
+    errorCode STRING,
+    errorMessage STRING,
+    requestParameters STRING,
+    responseElements STRING,
+    additionalEventData STRING,
+    requestId STRING,
+    eventId STRING,
+    resources ARRAY<STRUCT<
+        arn: STRING,
+        accountId: STRING,
+        type: STRING>>,
+    eventType STRING,
+    apiVersion STRING,
+    readOnly STRING,
+    recipientAccountId STRING,
+    serviceEventDetails STRING,
+    sharedEventID STRING,
+    vpcEndpointId STRING
+)
+COMMENT 'CloudTrail table for <MY_BUCKET> bucket'
+ROW FORMAT SERDE 'com.amazon.emr.hive.serde.CloudTrailSerde'
+STORED AS INPUTFORMAT 'com.amazon.emr.cloudtrail.CloudTrailInputFormat'
+OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://<MY_BUCKET>/AWSLogs/<MY_ACCOUNT_NUMBER>/'
+TBLPROPERTIES ('classification'='cloudtrail');
